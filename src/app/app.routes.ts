630921e9515b852ac90f8600bf78c0896c908239
Deepak <deepak@relinns.com>
2024-09-23 17:01:49 +0530

    [Added] - Root and child components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChildComponent } from './modules/child/child.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'child', component: ChildComponent},
+];
